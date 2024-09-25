0bad47bea6642530e9589e8fd57aaf9afcd93e99
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2024-09-25 12:59:45 -0700

    CODETOOLS-7903838: apidiff: Update Vintage JUnit test

--- test/junit/apitest/SerializationMethodsTest.java
+++ test/junit/apitest/SerializationMethodsTest.java
@@ -35,7 +35,7 @@ import java.util.stream.Stream;
 
 import jdk.codetools.apidiff.Main;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
